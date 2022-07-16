c16d92f5e4b83752250ccace6314323f57371d93
dmytro <dmytro>
2022-07-16 16:07:21 +0300

    TDD: parseTemplate: first test: another input value: red

--- src/test/java/com/hw/tdd/service/TemplateParserTest.java
+++ src/test/java/com/hw/tdd/service/TemplateParserTest.java
@@ -10,14 +10,19 @@ import java.util.List;
 public class TemplateParserTest {
 
     private static final String TEMPLATE1 = "Some text input 1: #{value1}, Some text input 2: #{value2}, Some text input 3: #{value3}.";
+    private static final String TEMPLATE2 = "Some text input 1: #{value21}, Some text input 2: #{value22}.";
 
     @Test
     public void getPlaceholdersFromTheTemplate() {
         TemplateParser parser = new TemplateParser();
-        List<String> placeholders = parser.parseTemplate(TEMPLATE1);
 
-        Assert.assertTrue(CollectionUtils.isNotEmpty(placeholders));
-        Assert.assertTrue(CollectionUtils.containsAll(placeholders, Arrays.asList("value1", "value2", "value3")));
+        List<String> placeholders1 = parser.parseTemplate(TEMPLATE1);
+        Assert.assertTrue(CollectionUtils.isNotEmpty(placeholders1));
+        Assert.assertTrue(CollectionUtils.containsAll(placeholders1, Arrays.asList("value1", "value2", "value3")));
+
+        List<String> placeholders2 = parser.parseTemplate(TEMPLATE2);
+        Assert.assertTrue(CollectionUtils.isNotEmpty(placeholders2));
+        Assert.assertTrue(CollectionUtils.containsAll(placeholders2, Arrays.asList("value21", "value22")));
     }
 
 }
