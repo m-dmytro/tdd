e8db71115026bd66d92bca76a3b2c0bbeb6538d3
dmytro <dmytro>
2022-07-16 16:20:09 +0300

    TDD: parseTemplate: first test: another input value: green

--- src/main/java/com/hw/tdd/service/TemplateParser.java
+++ src/main/java/com/hw/tdd/service/TemplateParser.java
@@ -2,13 +2,23 @@ package com.hw.tdd.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TemplateParser {
+
+    private static final String REGEX = "\\#\\{(?<placeholder>[A-Za-z0-9-_]+)}";
+
     public List<String> parseTemplate(String template) {
-        List placeholders = new ArrayList<>();
-        placeholders.add("value1");
-        placeholders.add("value2");
-        placeholders.add("value3");
+        List<String> placeholders = new ArrayList<>();
+
+        Pattern regex = Pattern.compile(REGEX);
+        Matcher regexMatcher = regex.matcher(template);
+
+        while (regexMatcher.find()) {
+            placeholders.add(regexMatcher.group(1));
+        }
         return placeholders;
     }
+
 }
